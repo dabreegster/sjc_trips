09e9c3c63fbd0f776f0d89b179deab4c8d724bad
Dustin Carlino <dabreegster@gmail.com>
2022-02-16 15:13:50 +0000

    Basic CLI input

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Initial installation:
 1. Install [poetry](https://python-poetry.org/)
 2. `poetry install`
 
-To run: `poetry run python main.py`
+To convert ticketing data to trips: `poetry run python main.py --bil_path ~/Downloads/Sharing_files/Data/bil_2019-09-01.csv`
 
 To format code: `poetry run black main.py`
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import csv
+import click
 
 
-def main():
-    print("TODO")
+@click.command()
+@click.option("--bil_path", required=True, help="path to a bilhetagem CSV file.")
+def ticketing_to_trips(bil_path):
+    with open(bil_path) as f:
+        for row in csv.DictReader(f):
+            print(row)
 
 
 if __name__ == "__main__":
-    main()
+    ticketing_to_trips()
